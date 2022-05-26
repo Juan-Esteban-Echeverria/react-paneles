383bf50ecb92e3e99cea1175b28a7ae24ec0efb5
Juan-Esteban-Echeverria <jeecheverria@uc.cl>
2022-05-26 14:02:23 -0400

    paneles reciclables

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+      <Testimonio 
+        nombre='Emma Bunster'
+        pais='Suecia'
+        imagen='emma'
+        cargo='Ingeniera de Software'
+        empresa='Spotify'
+        testimonio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis officiis, praesentium, in excepturi assumenda dolorem blanditiis porro fugit inventore commodi deleniti doloribus tempore natus hic voluptatem et animi error nihil soluta neque debitis nesciunt voluptatibus sapiente temporibus. Non doloribus provident corporis in modi, doloremque at totam blanditiis, veritatis voluptatum perspiciatis.'
+        />
+
+      <Testimonio 
+        nombre='Sarah Chima'
+        pais='Nigeria'
+        imagen='sarah'
+        cargo='Ingeniera de Software'
+        empresa='ChatDesk'
+        testimonio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis officiis, praesentium, in excepturi assumenda dolorem blanditiis porro fugit inventore commodi deleniti doloribus tempore natus hic voluptatem et animi error nihil soluta neque debitis nesciunt voluptatibus sapiente temporibus. Non doloribus provident corporis in modi, doloremque at totam blanditiis, veritatis voluptatum perspiciatis.'
+        />
+
+      <Testimonio 
+        nombre='Shawn Wang'
+        pais='Singapur'
+        imagen='shawn'
+        cargo='Ingeniero de Software'
+        empresa='Amazon'
+        testimonio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis officiis, praesentium, in excepturi assumenda dolorem blanditiis porro fugit inventore commodi deleniti doloribus tempore natus hic voluptatem et animi error nihil soluta neque debitis nesciunt voluptatibus sapiente temporibus. Non doloribus provident corporis in modi, doloremque at totam blanditiis, veritatis voluptatum perspiciatis.'
+        />
+
+      </div>
     </div>
   );
 }
